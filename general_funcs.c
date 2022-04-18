250dd1ff50c76fcb6669a71f6c9114a1325b0e23
Chukwuemeka Innocent <chukwuemeka140@gmail.com>
2022-04-18 14:24:47 +0100

    Add new branch with first code commit

---
+++ general_funcs.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int equal = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
+}
+
+/**
+ * @brief string_length - get the length of a string
+ * @string: string
+ * 
+ * Return: length in number
+ */
+
+int string_length(char *string)
+{
+    int size;
+
+    for (size = 0; string[size] != '\0'; size++)
+        ;
+    
+    return (size);
+}
---
+++ get_ops_name.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_ops - holds logic for pointing identifiers to functions
+ * Description - takes an identifier character and checks if we have a function to perform the operation
+ * @id: identifier string
+ * 
+ * Return: pointer to function that carries out the operation
+ */
+
+char *get_ops(char id)
+{
+    op_struct ops[] = {
+        {"d", "number"},
+        {"c", "character"},
+        {"s", "string"},
+        // {"i", op_numi},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (ops[i].id != NULL && *(ops[i].id) != id)
+        i++;
+    
+    return(ops[i].name);
+
+}
+
+
